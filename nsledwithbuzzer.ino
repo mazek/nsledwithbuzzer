a21b82e659a3259eb5ef45a4c1996738e9a8ad6e
Marcin Mazurek <m.mazurek@netsync.pl>
2017-09-16 22:03:54 +0200

    Reoarganized code + memore leak found.

--- nsledwithbuzzer.ino
+++ nsledwithbuzzer.ino
@@ -6,6 +6,8 @@
 #define DEBUG
 
 #include "config.h"
+#include "tools.h"
+
 
 int min = 80;
 int max = 160;
@@ -34,7 +36,11 @@ char read_time_s[13];
 unsigned long  cur_time;
 unsigned long  read_time;
 
-DynamicJsonBuffer  jsonBuffer;
+unsigned long sugar_level;
+long sugar_level_delta;
+
+unsigned long parakeet_last_seen;
+
 
 void setup() {
   pinMode(led_pin, OUTPUT);
@@ -80,12 +86,11 @@ void setColor(int red, int green, int blue)
   analogWrite(bluePin, blue);  
 }
 
-void loop() {
-  
-  rssi = WiFi.RSSI();  
-  DEBUG_PRINT("RSSI: ");
-  DEBUG_PRINTLN(rssi);
-  
+void parse_json(unsigned long&  cur_time, unsigned long&  read_time, unsigned long& sugar_level, long& sugar_level_delta, unsigned long& parakeet_last_seen) {
+
+  //DynamicJsonBuffer  jsonBuffer;
+  StaticJsonBuffer<1000>  jsonBuffer;
+    
   DEBUG_PRINTLN("making GET request");
   client.get("/pebble");
 
@@ -99,6 +104,13 @@ void loop() {
   DEBUG_PRINT("Response: -");
   DEBUG_PRINT(response);
   DEBUG_PRINTLN("-");
+
+//  int a = Node.Heap();
+//  DEBUG_PRINT("Free RAM: ");
+//  DEBUG_PRINTLN(a);
+
+
+//uint32_t free = system_get_free_heap_size();
       
   JsonObject& _data = jsonBuffer.parseObject(response);
 
@@ -128,10 +140,38 @@ void loop() {
   DEBUG_PRINT(", read_time: ");
   DEBUG_PRINTLN(read_time);
 
-  unsigned long parakeet_last_seen = cur_time - read_time ;
+  parakeet_last_seen = cur_time - read_time ;
   DEBUG_PRINT("I seen parakeet more then ");
   DEBUG_PRINT(parakeet_last_seen);
   DEBUG_PRINTLN(" seconds.");
+
+  if (parakeet_last_seen > 900) {
+    DEBUG_PRINTLN("I lost parakeet signal :(");
+  }
+  else {
+    // Parakeet operational.
+    DEBUG_PRINTLN("I got parakeet signal :)");
+    sugar_level = _data["bgs"][0]["sgv"];
+    DEBUG_PRINT("Sugar level: ");
+    DEBUG_PRINTLN(sugar_level);
+    sugar_level_delta = _data["bgs"][0]["bgdelta"];
+    DEBUG_PRINT("sugar_level_delta: ");
+    DEBUG_PRINTLN(sugar_level_delta);
+  }
+  //jsonBuffer.clear();
+  return;
+}
+
+void loop() {
+
+  DEBUG_PRINT("Free Heap: ");
+  DEBUG_PRINTLN(ESP.getFreeHeap());
+  
+  rssi = WiFi.RSSI();  
+  DEBUG_PRINT("RSSI: ");
+  DEBUG_PRINTLN(rssi);
+  
+  parse_json(cur_time, read_time, sugar_level, sugar_level_delta, parakeet_last_seen);
   
   if (parakeet_last_seen > 900) {
     DEBUG_PRINTLN("I lost parakeet signal :(");
@@ -139,29 +179,24 @@ void loop() {
   else {
     // Parakeet operational.
     DEBUG_PRINTLN("I got parakeet signal :)");
-    unsigned long bwpo = _data["bgs"][0]["sgv"];
-    DEBUG_PRINT("BWPO: ");
-    DEBUG_PRINTLN(bwpo);
-    long bgdelta = _data["bgs"][0]["bgdelta"];
-    DEBUG_PRINT("BGDELTA: ");
-    DEBUG_PRINTLN(bgdelta);
-
     
-    if (bgdelta > 0) {
+    if (sugar_level_delta > 0) {
       // Sugar is growing.
-      //      bgdelta_s = "+%s" % bgdelta
+      //      sugar_level_delta_s = "+%s" % sugar_level_delta
     }
     else {
       // Sugar is dropping.
-      //      bgdelta_s = "%s" % bgdelta
+      //      sugar_level_delta_s = "%s" % sugar_level_delta
     }
 
-    if (bwpo <= min-20) {
-      // Sugar below minimum level.
-      DEBUG_PRINT("Sugar: ");
-      DEBUG_PRINT(bwpo);
-      DEBUG_PRINT(", change:  ");
-      DEBUG_PRINTLN(bgdelta);
+    DEBUG_PRINT("Sugar: ");
+    DEBUG_PRINT(sugar_level);
+    DEBUG_PRINT(", change:  ");
+    DEBUG_PRINTLN(sugar_level_delta);
+    
+    if (sugar_level <= min-20) {
+      // Sugar way below minimum level.
+
       DEBUG_PRINTLN("Sugar below minimum level.");
       for (int x=0; x<5; x++ ) {
         digitalWrite(led_pin, HIGH);
@@ -172,12 +207,8 @@ void loop() {
       }   
       sugar_color = 1;  
     }    
-    else if (bwpo > min-20 and bwpo < min) {
+    else if (sugar_level > min-20 and sugar_level < min) {
       // Sugar below minimum level.
-      DEBUG_PRINT("Sugar: ");
-      DEBUG_PRINT(bwpo);
-      DEBUG_PRINT(", change:  ");
-      DEBUG_PRINTLN(bgdelta);
       DEBUG_PRINTLN("Sugar below minimum level.");
       for (int x=0; x<3; x++ ) {
         digitalWrite(led_pin, HIGH);
@@ -188,12 +219,8 @@ void loop() {
       }
       sugar_color = 2;     
     }
-    else if (bwpo > max) {
+    else if (sugar_level > max) {
       // Sugar above maximum level.
-      DEBUG_PRINT("Sugar: ");
-      DEBUG_PRINT(bwpo);
-      DEBUG_PRINT(", change:  ");
-      DEBUG_PRINTLN(bgdelta);
       DEBUG_PRINTLN("Sugar above maximum level.");
       for (int x=0; x<3; x++ ) {
         digitalWrite(led_pin, HIGH);
@@ -209,14 +236,14 @@ void loop() {
       sugar_color = 3; 
     }
     DEBUG_PRINTLN("-----");
-    DEBUG_PRINTLN(bwpo);
+    DEBUG_PRINTLN(sugar_level);
     DEBUG_PRINTLN("-----");
   }  
   DEBUG_PRINTLN("Wait fiveteen minutes");
   
-  // Blink every five minutes - I'm alive :)
-  for (int x=0; x<3; x++) {
-    for (int z=0; z<300; z++){
+  
+  for (int x=0; x<3; x++) { // Three sessions.
+    for (int z=0; z<30; z++){ // Blink the led with color according to sugar level.
       switch (sugar_color) {
         case 0:
           setColor(random(255), random(255), random(255));
@@ -228,7 +255,7 @@ void loop() {
           setColor(255, 255, 0);
           break;
         case 3:
-          setColor(0, 255, 0);
+          setColor(0, 15, 0);
           break;
         case 4:
           setColor(0, 0, 255);
@@ -239,7 +266,7 @@ void loop() {
       delay(300);
     }
          
-    for (int y=0; y<x+1; y++) {
+    for (int y=0; y<x+1; y++) { // Blink the led 1, 2 or 3 times to show where we are.
       digitalWrite(led_pin, HIGH);
       delay(300);                      
       digitalWrite(led_pin, LOW);  
